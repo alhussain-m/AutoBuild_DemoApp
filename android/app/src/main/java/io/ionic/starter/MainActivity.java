060a3562d80873e6330f618c6f3c273ed88da330
Automation <automation@Automations-Mac-mini.local>
2025-05-14 16:50:37 +0530

    first commit

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.citus.stage;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.citus.stage',
   appName: 'AutoBuild_DemoApp',
   webDir: 'www'
 };
