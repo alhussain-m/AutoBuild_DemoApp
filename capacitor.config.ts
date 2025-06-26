02f767b379bc963b87dcf8fcf106d87fc7d5a4e8
Automation <automation@Automations-Mac-mini.local>
2025-06-26 10:19:54 +0530

    plist change 26-6-25/1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,18 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+// import type { CapacitorConfig } from '@capacitor/cli';
+
+// const config: CapacitorConfig = {
+//   appId: 'com.citus.stage',
+//   appName: 'AutoBuild_DemoApp',
+//   webDir: 'www'
+// };
+
+// export default config;
+
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.citus.stage',
   appName: 'AutoBuild_DemoApp',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false // Recommended for modern builds
 };
-
-export default config;
